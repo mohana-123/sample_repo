420bb6920dab79177e4a0d7e85e77118113fab05
Mohana Sruthy Mada <104384297+mohana-123@users.noreply.github.com>
2024-08-02 20:07:33 +0530

    Update uploadService.js

--- frontend/src/services/uploadService.js
+++ frontend/src/services/uploadService.js
@@ -1,39 +1,38 @@
-import axios from "axios";
-import { toast } from "react-toastify";
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
 export const uploadImage = async event => {
-    let toastId = null;
+  let toastId = null;
 
-    const image = await getImage(event);
-    if (!image) return null;
+  const image = await getImage(event);
+  if (!image) return null;
 
-    const formData = new FormData();
-    formData.append('image', image, image.name);
-    const response = await axios.post('api/upload', formData, {
-        onUploadProgress: ({ progress }) => {
-            if (toastId) toast.update(toastId, { progress });
-            else toastId = toast.success('Uploading...', { progress });
-        },
-    });
-    toast.dismiss(toastId);
-    return response.data.imageUrl;
+  const formData = new FormData();
+  formData.append('image', image, image.name);
+  const response = await axios.post('api/upload', formData, {
+    onUploadProgress: ({ progress }) => {
+      if (toastId) toast.update(toastId, { progress });
+      else toastId = toast.success('Uploading...', { progress });
+    },
+  });
+  toast.dismiss(toastId);
+  return response.data.imageUrl;
 };
 
 const getImage = async event => {
-    const files = event.target.files;
+  const files = event.target.files;
 
-    if (!files || files.length <= 0) {
-        toast.warning('Upload file is not selected!', 'File Upload');
-        return null;
-    }
+  if (!files || files.length <= 0) {
+    toast.warning('Upload file is nott selected!', 'File Upload');
+    return null;
+  }
 
-    const file = files[0];
+  const file = files[0];
 
-    if (file.type !== 'image/jpeg') {
-        toast.error('only JPG type is allowed', 'File type Error');
-        return null;
-    }
+  if (file.type !== 'image/jpeg') {
+    toast.error('Only JPG type is allowed', 'File Type Error');
+    return null;
+  }
 
-    return file;
-
+  return file;
 };
