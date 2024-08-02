4f558d0e5167ebc342d9592cea7388af1ae37edb
madamohana143@gmail.com <madamohana143@gmail.com>
2024-08-02 20:22:56 +0530

    now

--- backend/src/routers/upload.router.js
+++ backend/src/routers/upload.router.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
 import admin from '../middleware/admin.mid.js';
-import multer from "multer";
-import handler from "express-async-handler";
-import { BAD_REQUEST } from "../constants/httpStatus.js";
-import { configCloudinary } from "../config/cloudinary.config.js";
+import multer from 'multer';
+import handler from 'express-async-handler';
+import { BAD_REQUEST } from '../constants/httpStatus.js';
+import { configCloudinary } from '../config/cloudinary.config.js';
 
 const router = Router();
 const upload = multer();
