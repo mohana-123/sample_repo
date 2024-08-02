13d6d63319dce02478c558eb8ada0559c8f32f26
Mohana Sruthy Mada <104384297+mohana-123@users.noreply.github.com>
2024-08-02 20:09:25 +0530

    Update FoodEditPage.js

--- frontend/src/pages/FoodEdit/FoodEditPage.js
+++ frontend/src/pages/FoodEdit/FoodEditPage.js
@@ -2,98 +2,116 @@ import { useParams } from 'react-router-dom';
 import classes from './foodEdit.module.css';
 import { useForm } from 'react-hook-form';
 import { useEffect, useState } from 'react';
-import { getById } from '../../services/foodService';
+import { add, getById, update } from '../../services/foodService';
 import Title from '../../components/Title/Title';
 import InputContainer from '../../components/InputContainer/InputContainer';
 import Input from '../../components/Input/Input';
 import Button from '../../components/Button/Button';
 import { uploadImage } from '../../services/uploadService';
-
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 export default function FoodEditPage() {
-    const { foodId } = useParams();
-    const [imageUrl, setImageUrl] = useState();
-    const isEditMode = !!foodId;
+  const { foodId } = useParams();
+  const [imageUrl, setImageUrl] = useState();
+  const isEditMode = !!foodId;
 
-    const {
-        handleSubmit,
-        register,
-        formState: { errors },
-        reset,
-    } = useForm();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (isEditMode) return;
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-        getById(foodId).then(food => {
-            if (!food) return;
-            reset(food);
-            setImageUrl(food.imageUrl);
-        })
-    }, [foodId]);
+  useEffect(() => {
+    if (!isEditMode) return;
 
-    const submit = () => { };
+    getById(foodId).then(food => {
+      if (!food) return;
+      reset(food);
+      setImageUrl(food.imageUrl);
+    });
+  }, [foodId]);
 
-    const upload = async event => {
-        setImageUrl(null);
-        const imageUrl = await uploadImage(event);
-        setImageUrl({imageUrl});
-    };
+  const submit = async foodData => {
+    const food = { ...foodData, imageUrl };
 
+    if (isEditMode) {
+      await update(food);
+      toast.success(`Food "${food.name}" updated successfully!`);
+      return;
+    }
 
-    return (
-        <div className={classes.container}>
-            <div className={classes.content}>
-                <Title title={isEditMode ? 'Edit Food' : 'Add Food'} />
-                <form onSubmit={handleSubmit(submit)} noValidate>
-                    <InputContainer label="Select Image">
-                        <input type='file' onChange={upload} accept='image/jpeg' />
-                    </InputContainer>
+    const newFood = await add(food);
+    toast.success(`Food "${food.name}" added successfully!`);
+    navigate('/admin/editFood/' + newFood.id, { replace: true });
+  };
 
-                    {imageUrl && (
-                        <a href={imageUrl} className={classes.image_link} target='blank'>
-                            <img src={imageUrl} alt='Uploaded' />
-                        </a>
-                    )}
+  const upload = async event => {
+    setImageUrl(null);
+    const imageUrl = await uploadImage(event);
+    setImageUrl(imageUrl);
+  };
 
-                    <Input
-                        type="text"
-                        label="Name"
-                        {...register('name', { required: true, minLength: 5 })}
-                        error={errors.name}
-                    />
+  return (
+    <div className={classes.container}>
+      <div className={classes.content}>
+        <Title title={isEditMode ? 'Edit Food' : 'Add Food'} />
+        <form
+          className={classes.form}
+          onSubmit={handleSubmit(submit)}
+          noValidate
+        >
+          <InputContainer label="Select Image">
+            <input type="file" onChange={upload} accept="image/jpeg" />
+          </InputContainer>
 
-                    <Input
-                        type="number"
-                        label="Price"
-                        {...register('price', { required: true })}
-                        error={errors.price}
-                    />
+          {imageUrl && (
+            <a href={imageUrl} className={classes.image_link} target="blank">
+              <img src={imageUrl} alt="Uploaded" />
+            </a>
+          )}
 
-                    <Input
-                        type="text"
-                        label="Tags"
-                        {...register('tags')}
-                        error={errors.tags}
-                    />
+          <Input
+            type="text"
+            label="Name"
+            {...register('name', { required: true, minLength: 5 })}
+            error={errors.name}
+          />
 
-                    <Input
-                        type="text"
-                        label="Origins"
-                        {...register('origins', { required: true })}
-                        error={errors.origins}
-                    />
+          <Input
+            type="number"
+            label="Price"
+            {...register('price', { required: true })}
+            error={errors.price}
+          />
 
-                    <Input
-                        type="text"
-                        label="Cook Time"
-                        {...register('cookTime', { required: true })}
-                        error={errors.cookTime}
-                    />
+          <Input
+            type="text"
+            label="Tags"
+            {...register('tags')}
+            error={errors.tags}
+          />
 
-                    <Button type="submit" text={isEditMode ? 'Update' : 'Create'} />
-                </form>
-            </div>
-        </div>
-    )
+          <Input
+            type="text"
+            label="Origins"
+            {...register('origins', { required: true })}
+            error={errors.origins}
+          />
+
+          <Input
+            type="text"
+            label="Cook Time"
+            {...register('cookTime', { required: true })}
+            error={errors.cookTime}
+          />
+
+          <Button type="submit" text={isEditMode ? 'Update' : 'Create'} />
+        </form>
+      </div>
+    </div>
+  );
 }
